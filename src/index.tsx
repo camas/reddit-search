4a83879f995bb1f5e1d1b1396a4425fcf985ec71
camas <camas@users.noreply.github.com>
2020-04-22 07:43:44 +0100

    Add error display
    also a hack

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {App} from './app';
+import { App } from './app';
+import { ErrorWrapper } from './error';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+ReactDOM.render(<ErrorWrapper><App /></ErrorWrapper>, document.getElementById('react-root'));
